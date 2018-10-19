c13080b751ef8fdd435cc0ac43e286dab663c993
Nicholas Landau <nrlandau@gmail.com>
2018-10-19 16:34:29 -0400

    created Circle class and fuctions

--- Program.cs
+++ Program.cs
@@ -2,11 +2,34 @@
 
 namespace Lab10
 {
+    class Circle
+    {
+        //vars
+        double radius;
+        //constructer
+        public Circle()
+        {
+
+        }
+        //methods
+        public double CalculateCircumference(){ return 0;}
+        public string CalculateFormattedCircumference(){return "";}
+        public double CalculateArea(){return 0;}
+        public string CalculateFormattedArea(){return "";}
+        private string FormatNumber(double x){return "";}
+        //Propertys
+        public double Radius
+        {
+            get{return radius;}
+            set{radius = value;}
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
         }
     }
 }
